c0af84f644e0b0b484911e03bfd8e9b6b388dda2
Petre Ioan Iulian <ioan_iulian.petre@stud.fiir.upb.ro>
2025-10-06 21:36:37 +0300

    Caculate Error Function

--- Mirositor.cpp
+++ Mirositor.cpp
@@ -14,7 +14,7 @@ MotorShield motors(M1DIR, M1PWM, M2DIR, M2PWM);
 #define SensorCount 13
 const int sensorPins = {};
 
-int sensorWeights = {};
+int sensorWeights[SensorCount] = {-600, -500, -400, -300, -200, -100, 0, 100, 200, 300, 400, 500, 600};
 
 //PID Config
 double Kp = 0;
@@ -23,6 +23,28 @@ double Ki = 0;
 
 double baseSpeed = 25;
 double error = 0, lastError = 0, integral = 0;
+
+double calculateError () {
+    double weightSum = 0;
+    short int activeCount = 0;
+
+    for (int i = 0; i < SensorCount; i++) {
+      if(digitalRead(sensorPins)) {
+        weightSum += sensorWeights[i];
+        activeCount++;
+      }
+    }
+
+    if (activeCount != 0) {
+      error = weightSum /(float)activeCount;
+    }
+
+    // adjustable field to toy with
+    if (error < 0.1) {
+      error = 0;
+    }
+}
+
 void setup() {
 
 
--- MotorShield.h
+++ MotorShield.h
@@ -10,31 +10,32 @@
 #include "DRI0040_Motor.h"
 
 class MotorShield {
-  public:
-    MotorShield(uint8_t M1Dir, uint8_t M1PWM, bool M1invert = false,
-                        uint8_t M2Dir, uint8_t M2PWM, bool M2invert = false)
-    :M1 (M1Dir, M1PWM),
-     M2 (M2Dir, M2PWM) {}
+public:
+  MotorShield(uint8_t M1Dir, uint8_t M1PWM,
+              uint8_t M2Dir, uint8_t M2PWM,
+              bool M1invert = false, bool M2invert = false)
+  : M1 (M1Dir, M1PWM, M1invert),
+    M2 (M2Dir, M2PWM, M2invert) {}
 
-   void begin() {
-     M1.begin();
-     M2.begin();
-   }
+  void begin() {
+    M1.begin();
+    M2.begin();
+  }
 
-   void setM1speed(int speed) {
-     M1.set_speed(speed);
-   }
+  void setM1speed(int speed) {
+    M1.set_speed(speed);
+  }
 
-   void setM2speed(int speed) {
-     M2.set_speed(speed);
-   }
+  void setM2speed(int speed) {
+    M2.set_speed(speed);
+  }
 
-   void stop() {
-     M1.stop();
-     M2.stop();
-   }
+  void stop() {
+    M1.stop();
+    M2.stop();
+  }
 
-  private: DRI0040_Motor M1;
-           DRI0040_Motor M2;
+private: DRI0040_Motor M1;
+  DRI0040_Motor M2;
 };
 #endif //DRI0040__MOTORSHIELD_H
