c0ea949100bfef6a4ba8a2c903802e9e4c118c06
Petre Ioan Iulian <ioan_iulian.petre@stud.fiir.upb.ro>
2025-10-06 22:41:53 +0300

    Verified and Compiled by Arduino IDE

--- DRI0040_Motor.h
+++ DRI0040_Motor.h
@@ -1,41 +1,40 @@
-//
-// Created by Petre Ioan Iulian on 10/6/2025.
-//
-
 #ifndef DRI0040_MOTOR_H
 #define DRI0040_MOTOR_H
 
 #pragma once
 #include <Arduino.h>
-#include <stdint.h>
 
 class DRI0040_Motor {
-  public:
-    DRI0040_Motor(uint8_t Dir, uint8_t PWM, bool invert = false);
+public:
+  DRI0040_Motor(uint8_t Dir, uint8_t PWM, bool invert = false)
+    : Dir(Dir), PWM(PWM), invert(invert) {}
 
-    void begin(){
-      pinMode(Dir, OUTPUT);
-      pinMode(PWM, OUTPUT);
-      stop();
-    }
+  void begin() {
+    pinMode(Dir, OUTPUT);
+    pinMode(PWM, OUTPUT);
+    stop();
+  }
 
   void stop() {
-  	analogWrite(Dir, 0);
+    digitalWrite(Dir, LOW);    
+    analogWrite(PWM, 0);
   }
 
   void set_speed(int speedPercentage) {
     speedPercentage = constrain(speedPercentage, -100, 100);
 
     const int speed = map(abs(speedPercentage), 0, 100, 0, 255);
-    const bool forward = (speedPercentage < 0);
+    const bool forward = (speedPercentage > 0);
 
     digitalWrite(Dir, (forward ^ invert) ? LOW : HIGH);
 
+    analogWrite(PWM, speed);
   }
 
-  private:
-    uint8_t Dir;
-    uint8_t PWM;
-    bool invert;
+private:
+  uint8_t Dir;    
+  uint8_t PWM;    
+  bool invert;    
 };
-#endif //DRI0040_MOTOR_H
+
+#endif // DRI0040_MOTOR_H
