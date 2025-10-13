148cdb30a478ac859ca023ae0324622171f8e219
Petre Ioan Iulian <ioan_iulian.petre@stud.fiir.upb.ro>
2025-10-13 22:14:09 +0300

    Final Version I hope

--- Mirositor.cpp
+++ Mirositor.cpp
@@ -1,15 +1,18 @@
 #include "MotorShield.h"
 #include "DRI0040_Motor.h"
 #include "QTRSensors.h"
+#include <IRremote.h>
 
 //Flags
 #define DEBUG_FLAG false
 #define CALIBRATION_FLAG true
 
-//IR Reciver Configuration
+//IR Config
 bool robot_state = true;
+#define IR_PIN 36
 
 #define MAXSPEED 100
+#define MAXOUTPUT 50
 #define SensorCount 8
 
 //Motor Config
@@ -25,24 +28,47 @@ const uint8_t sensorPins[SensorCount] = {5, 8, 25, 26, 12, 15, 14, 13};
 uint16_t sensorValues[SensorCount];
 
 //PID values
-double Kp = 0.05;
+double Kp = 0.1;
 double Kd = 0.8;
 double Ki = 0;
 
 
 double baseSpeed = 25;
-int lastError = 0;
+double lastError = 0;
 double integral = 0;
+double error = 0, error1 = 0, error2 = 0, error3 = 0, error4 = 0, error5 = 0, error6 = 0;
 
-int calculateError() {
-  int position = qtr.readLineBlack(sensorValues); // 0 (left) -> 7000 (right)
-  int error = position - 3500;
+void start_stop() {
+   if (IrReceiver.decode()) {
+        uint8_t cmd = IrReceiver.decodedIRData.command;
+
+        if (cmd == 0x45) {
+            robot_state = 1;
+        } else if (cmd == 0x46) {
+            robot_state = 0;
+        } else if (cmd == 0x47) {
+            robot_state = 1;
+        }
+
+        IrReceiver.resume();
+     }
+}
+
+double calculateError() {
+  double position = qtr.readLineBlack(sensorValues); // 0 (left) -> 7000 (right)
+  error = 3500 - position;
+  error6 = error5;
+  error5 = error4;
+  error4 = error3;
+  error3 = error2;
+  error2 = error1;
+  error1 = error;
   return error;
 }
 
 //PID Function
 double PID(int error) {
-  integral += error;
+  integral = error6 + error5 + error4 + error3 + error2 + error1 + error;
   double derivative = error - lastError;
   lastError = error;
 
@@ -60,7 +86,7 @@ void calibrate() {
     delay(10);
   }
 
-  Serial.println("-----Calibration Completed");
+  Serial.println("-----Calibration Completed-----");
 
   for (uint8_t i = 0; i < SensorCount; i++) {
     Serial.print("Sensor ");
@@ -103,8 +129,10 @@ void setup() {
 
   Serial.println("Robot ON");
 
-  if (CALIBRATION_FLAG) {
+  if (CALIBRATION_FLAG && Serial) {
     Serial.begin(115200);
+  }
+  else if (CALIBRATION_FLAG) {
     calibrate();
   }
 
@@ -112,13 +140,15 @@ void setup() {
     Serial.begin(115200);
     debug();
   }
+
+  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
 }
 
 void loop() {
   if (!robot_state) return;
 
-  int error = calculateError();
-  double correction = constrain(PID(error), -MAXSPEED, MAXSPEED);
+  error = calculateError();
+  double correction = constrain(PID(error), -MAXOUTPUT, MAXOUTPUT);
 
   double left = baseSpeed - correction;
   double right = baseSpeed + correction;
@@ -129,14 +159,12 @@ void loop() {
   motors.setM1speed(left);
   motors.setM2speed(right);
 
-  Serial.print("Error: ");
-  Serial.print(error);
-  Serial.print("\tCorection: ");
-  Serial.print(correction);
-  Serial.print("\tL: ");
-  Serial.print(left);
-  Serial.print("\tR: ");
-  Serial.println(right);
+  if(Serial) {
+    Serial.print("Error: "); Serial.print(error);
+    Serial.print("\tCorection: "); Serial.print(correction);
+    Serial.print("\tL: "); Serial.print(left);
+    Serial.print("\tR: "); Serial.println(right);
+  }
 
   delay(10);
 }
